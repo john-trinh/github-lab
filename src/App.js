d2cc046213f21a137d3bb9c497787de5c5a9a9be
Nguyen.tlc <nguyen.tlc@sutrixsolutions.com>
2018-08-08 17:14:20 +0700

    horizontal tab

--- src/App.js
+++ src/App.js
@@ -5,18 +5,41 @@ import Tab from './Tab';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabs: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      tabs: [
+        ["a", ["A1", "A2", "A3"]],
+        ["b" , ["B1", "B2", "B3"]],
+        ["c", ["C1", "C2", "C3"]]
+      ]
+    })
+  }
   render() {
+    const tabs = this.state.tabs.map((t) => {
+      const [k, h] = t;
+      const inner = h.map((k2) => {
+        return (<Tab name={k2} key={k2}>
+          <h1>{k2}</h1>
+        </Tab>);
+      });
+      return (
+        <Tab name={k} key={k}>
+          <TabList vertical key={k}>
+            {inner}
+          </TabList>
+        </Tab>
+      );
+    });
     return (
-      <TabList>
-        <Tab name="a">
-          <h1>HelloA</h1>
-        </Tab>
-        <Tab name="b">
-          <h1>HelloB</h1>
-        </Tab>
-        <Tab name="c">
-          <h1>HelloC</h1>
-        </Tab>
+      <TabList horizontal>
+        {tabs}
       </TabList>
     );
   }
--- src/TabList.js
+++ src/TabList.js
@@ -4,14 +4,37 @@ export default class TabList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        selected: 'a'
+        selected: null
     };
   }
+
+  select(item) {
+    this.setState({
+        selected: item
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.selected == null) {
+      let defaultTab = React.Children.toArray(nextProps.children).map((child) => child.props.name)[0];
+
+      React.Children.forEach(nextProps.children, (child) => {
+        if (child.props.default) {
+          defaultTab = child.props.name;
+        }
+      });
+
+      this.setState({
+        selected: defaultTab
+      });
+    }
+  }
+
   render() {
     const tabs = React.Children.map(this.props.children, (child) => {
       const className = (child.props.name === this.state.selected) ? "selected" : "unselected";
       return (
-        <h1 className={className}>{child.props.name}</h1>
+        <h1 className={className} onClick={(e) => this.select(child.props.name)}>{child.props.name}</h1>
       );
     });
 
@@ -21,12 +44,13 @@ export default class TabList extends React.Component {
         body = child;
       }
     });
+    const direction = this.props.horizontal ? "horizontal" : "vertical";
     return (
-      <div className="holder">
-        <div className="tabs">
+      <div className={`holder ${direction}`}>
+        <div className={`tabs ${direction}`}>
           {tabs}
         </div>
-        <div className="body">
+        <div className={`body ${direction}`}>
           {body}
         </div>
       </div>
